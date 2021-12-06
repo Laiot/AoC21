e8a78aa3514d2e46aab2c51cb81f57a13d634033
Laiot <carmelo.sarta.main@gmail.com>
2021-12-06 08:59:29 +0100

    Pushed day 6.

--- day6/day6.go
+++ day6/day6.go
@@ -39,29 +39,44 @@ func HandleIntegerInputFile(path string) []int {
 	return PutInputIntoIntArray(file)
 }
 
-func NextDay(state *[]int) {
-	for idx := range *state {
-		if (*state)[idx] == 0 {
-			(*state)[idx] = 6
-			*state = append(*state, 8)
-		} else {
-			(*state)[idx]--
+func FishProgression(init_state *[]int, days int) int {
+	state := make(map[int]int, 8)
+
+	for _, i := range *init_state {
+		state[i] += 1
+	}
+
+	for day := 0; day < days; day++ {
+		pastState := map[int]int{}
+		for i, v := range state {
+			pastState[i] = v
+		}
+		for i := 0; i < 9; i++ {
+			switch i {
+			case 8:
+				state[8] = pastState[0]
+			case 6:
+				state[6] = pastState[7] + pastState[0]
+			default:
+				state[i] = pastState[i+1]
+			}
 		}
 	}
-}
 
-func SkipTime(state *[]int, days int) {
-	for i := 0; i < days; i++ {
-		NextDay(state)
+	sum := 0
+	for _, i := range state {
+		sum += i
 	}
+	return sum
 }
 
 func D6Part1() {
 	init_state := HandleIntegerInputFile("day6/in6.txt")
-	SkipTime(&init_state, 80)
-	fmt.Println(len(init_state))
+	fmt.Println(FishProgression(&init_state, 80))
+
 }
 
 func D6Part2() {
-
+	init_state := HandleIntegerInputFile("day6/in6.txt")
+	fmt.Println(FishProgression(&init_state, 256))
 }
